95a96f4b5c20e81de6692ff7f330ba55a6b26803
AMIT KUMAR <113933332+amitraj99053@users.noreply.github.com>
2026-04-03 13:46:17 +0530

    Final synchronization of all UI components

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -89,12 +89,12 @@ const Map = ({ pickup, destination, driver }) => {
         <MapContainer 
             center={defaultCenter} 
             zoom={13} 
-            style={{ width: '100%', height: '100%', background: '#1a1a1a' }}
+            style={{ width: '100%', height: '100%' }}
             zoomControl={false}
         >
             <TileLayer
-                url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
             
             <MapUpdater pickup={pickup} destination={destination} driver={driver} />
